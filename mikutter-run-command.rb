417a57c2e55ee18b520070163786b92a6569689d
Toshiaki Asai <toshi.alternative@gmail.com>
2013-04-01 09:11:23 +0900

    delete setting check routine

--- mikutter-run-command.rb
+++ mikutter-run-command.rb
@@ -2,36 +2,19 @@
 
 Plugin.create :run_com do
 
-  def main
-    (UserConfig[:runcom]|| []).select{|m|!m.empty?}.each do |str|
-      name="コマンド実行 #{str}"
-      slug=":#{str}"
-      command(slug.to_sym,
-              name: name,
-              condition: lambda{|opt| true},
-              visible: false,
-              role: :window) do |opt|
-        Thread.new {
-          system(str)
-        }
-      end
+  (UserConfig[:runcom]|| []).select{|m|!m.empty?}.each do |str|
+    name="コマンド実行 #{str}"
+    slug=":#{str}"
+    command(slug.to_sym,
+            name: name,
+            condition: lambda{|opt| true},
+            visible: false,
+            role: :window) do |opt|
+      Thread.new {
+        system(str)
+      }
     end
   end
-
-#もっとスマートに書けるはず
-#1秒毎に設定を確認してcommandに登録
-  def sub
-    old=(UserConfig[:runcom]|| []).select{|m|!m.empty?}
-    Reserver.new(1){
-      if (UserConfig[:runcom]|| []).select{|m|!m.empty?}!=old
-        main
-      end
-      sub
-    }
-  end  
-
-  main
-  sub
 
   settings "コマンド実行" do
     settings "コマンドの指定" do
